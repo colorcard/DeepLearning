18f2e25a9f3485df7f1059e326c8e9b24a9fe4f3
colorcard <liwq6688@icloud.com>
2025-03-26 12:52:18 +0800

    线性模型修改

--- linear-regression.py
+++ linear-regression.py
@@ -7,10 +7,6 @@ true_w = torch.tensor([2, -3.4])
 true_b = 4.2
 features, labels = d2l.synthetic_data(true_w, true_b, 1000)
 
-d2l.set_figsize()
-plt.scatter(features[:, (1)].detach().numpy(), labels.detach().numpy(), 1)
-plt.show()
-
 def data_iter(batch_size, features, labels):
     num_examples = len(features)
     indices = list(range(num_examples))
@@ -25,19 +21,35 @@ batch_size = 10
 w = torch.normal(0, 0.01, size=(2, 1), requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
 
+def linreg(X, w, b):
+    return torch.matmul(X, w) + b
+
+def squared_loss(y_hat, y):
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
 lr = 0.03
 num_epochs = 3
-net = d2l.linreg
-loss = d2l.squared_loss
+net = linreg
+loss = squared_loss
+
+def sgd(params, lr, batch_size):
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
 
 for epoch in range(num_epochs):
     for X, y in data_iter(batch_size, features, labels):
         l = loss(net(X, w, b), y)
         l.sum().backward()
-        d2l.sgd([w, b], lr, batch_size)
+        sgd([w, b], lr, batch_size)
     with torch.no_grad():
         train_l = loss(net(features, w, b), labels)
         print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
+
+d2l.set_figsize()
+plt.scatter(features[:, (1)].detach().numpy(), labels.detach().numpy(), 1)
+plt.show()
 
 print(f'w: {w.reshape(true_w.shape)},b: {b}')
 print(f'w的估计误差: {true_w - w.reshape(true_w.shape)}')
